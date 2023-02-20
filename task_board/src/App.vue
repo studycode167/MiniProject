a4307534056be18079e5f2f75728e0d7b66f21a4
Yangyungi <77335708+studycode167@users.noreply.github.com>
2023-02-20 18:02:22 +0900

    TIL
    nodejs study

---
+++ task_board/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Login></Login>
+</template>
+
+<script>
+import Login from "./views/Login.vue";
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+  components : {
+    Login,
+  }
+};
+</script>
